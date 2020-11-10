f8889e7243fffdbb7415ef7d3d452d3feec90474
Fernando Chavez <fjchavez01@gmail.com>
2020-11-10 12:46:45 -0800

    made form and added routes

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import './App.css'
+
+import Login from './Components/Login'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route exact path='/'>
+            <h1>Home</h1>
+          </Route>
+          <Route path='/login' component={Login}/>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/Components/Login.js
@@ -0,0 +1,48 @@
+import React, { Component }  from 'react'
+import axios from 'axios'
+
+class Login extends Component {
+
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    }
+
+    onChange = e => {
+        this.setState({
+          credentials: {
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+          }
+        });
+        console.log(this.state.credentials)
+      };
+
+    onSubmit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', this.state.credentials)
+    .then(req => {
+        console.log(req.data)
+        localStorage.setItem("token", req.data.payload);
+    })
+    .catch( err => {
+        console.log(err)
+    });
+    };
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <input type='text' value={this.state.credentials.username} name='username' onChange={this.onChange} placeholder='username'/>
+                    <input type='text' value={this.state.credentials.password} name='password' onChange={this.onChange} placeholder='password'/>
+                    <button>Login</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Login
